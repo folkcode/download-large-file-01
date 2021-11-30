2ffe70a645541fa25a93c3934aeb8cfe2e63df84
Huy Lu <Ext.Huy.LuPhucQuang@achilles.com>
2021-11-30 12:33:45 +0700

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace download_large_file_01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        [Route("download-zip")]
+        public async Task<IActionResult> DownloadMultiFilesAsync([FromBody] List<object> files)
+        {
+            // TODO: Testing purpose
+
+            var stream = new System.IO.FileStream("download-large-file-01.zip", System.IO.FileMode.Open, System.IO.FileAccess.Read);
+
+            return File(stream, "application/zip", "download-large-file-01.zip");
+        }
+    }
+}
